4968d8358502ee336f90c5a3c99ea7af6f6f0b1f
ricardodalcorso <rickcorso@gmail.com>
2025-02-04 20:04:18 -0300

    Update a1.py

--- estudos/a1.py
+++ estudos/a1.py
@@ -1,3 +1,13 @@
-def validate(x, y):
-    if isinstance(x, int) and isinstance(y, int):
-        return x / y
+from dash import Dash
+from dash_html_components import Div, H1, P
+
+app = Dash(__name__)
+
+app.layout = Div(
+    children=[
+        H1("ola mundo"), 
+        P('Bem vindo ao dash2_')
+    ]
+)
+
+app.run_server(debug = True) 
