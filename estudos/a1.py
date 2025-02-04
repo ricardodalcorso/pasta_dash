1ec7f51cf24d3177fe5226421d20232d6447e8ba
ricardodalcorso <rickcorso@gmail.com>
2025-02-04 20:45:56 -0300

    1

--- estudos/a1.py
+++ estudos/a1.py
@@ -1,13 +1,1 @@
-from dash import Dash
-from dash_html_components import Div, H1, P
-
-app = Dash(__name__)
-
-app.layout = Div(
-    children=[
-        H1("ola mundo"), 
-        P('Bem vindo ao dash2_')
-    ]
-)
-
-app.run_server(debug = True) 
+3 > 3
