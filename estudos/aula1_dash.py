45493076fd3f26098dc3af6743d9be62797192bd
ricardodalcorso <rickcorso@gmail.com>
2025-02-04 10:18:46 -0300

    1

---
+++ estudos/aula1_dash.py
@@ -0,0 +1,13 @@
+from dash import Dash
+from dash_html_components import Div, H1, P
+
+app = Dash(__name__)
+
+app.layout = Div(
+    children=[
+        H1("ola mundo"), 
+        P('Bem vindo ao dash2')
+    ]
+)
+
+app.run_server(debug = True) 
